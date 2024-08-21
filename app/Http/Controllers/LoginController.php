a91b2838dd803aacbb8f145ce12c9285de00658e
Arian <arianaghaee3@gmail.com>
2024-08-21 09:40:29 +0330

    login and register created

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use JetBrains\PhpStorm\NoReturn;
+
+class LoginController extends Controller
+{
+
+
+    public function login(Request $request)
+    {
+        $valid = validator::validate($request->all(), [
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+//        if ($valid->fails()) {
+//            return response()->json($valid->errors(), 422);
+//        }
+
+        $cridentials = $request->only('email', 'password');
+
+
+        if(!Auth::attempt($cridentials)){
+            return response()->json([
+                'status'=>'error',
+                'message'=>'unauthorized'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('authToken')->plainTextToken;
+        return response()->json([
+            'status'=> 'success',
+            'user'=> $user,
+            'authorisation'=> [
+                'token' => $token,
+                'type' => 'bearer'
+            ]
+        ]);
+
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate( [
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'username'=>'required|unique:users',
+            'password'=>'required'
+        ]);
+
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'message'=>'User Registered Successfully',
+            'user'=>$user,
+            'authorisation'=> [
+                'token' => $token,
+                'type' => 'bearer'
+            ]
+        ]);
+    }
+
+    public function userDetails()
+    {
+        return response()->json(auth()->user());
+    }
+}
